aded6d678e09ed4f45d358c42c799aa275a64032
Pedro H Birais <phbirais@gmail.com>
2020-06-05 23:24:27 -0300

    Day 5 - NLW:  DB and Register

--- NextLevelWeek/public/scripts/create-point.js
+++ NextLevelWeek/public/scripts/create-point.js
@@ -70,6 +70,8 @@ function handleSelectedItem(event){
 
     const itemId = itemLi.dataset.id
 
+    console.log('ITEM ID: ', itemId)
+
     //verificar se existem itens selecionados, se sim:
     //pegar os itens selecionados
     const alreadySelected = selectedItems.findIndex(item => { //executa uma função anonima em cada item
@@ -90,7 +92,11 @@ function handleSelectedItem(event){
         //adicionar a seleção
         selectedItems.push(itemId)
     }
-    console.log(selectedItems)
+    
+    //console.log(selectedItems)
+    //console.log('selectedItems: ', selectedItems)
+
+
     //atualizar o campo escondido com os itens selecionados
     collectedItems.value = selectedItems
 }
